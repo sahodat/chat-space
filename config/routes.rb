e32c02df44b2848b9014649745770e6a1750b054
sahodat <7207t251s906@gmail.com>
2018-11-02 14:28:15 +0900

    make group

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'messages#index'
+  devise_for :users
+  # root 'messages#index'
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
+
+
