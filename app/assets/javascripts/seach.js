b4a32cb54b637ccda42c3dd4f3f89c951380f3ad
sahodat <7207t251s906@gmail.com>
2018-11-07 16:53:45 +0900

    asynchronous search

--- app/assets/javascripts/seach.js
+++ app/assets/javascripts/seach.js
@@ -2,5 +2,11 @@ $(function() {
   $('#user-search-field').on("keyup", function() {
     var input = $('#user-search-field').val();
     console.log(this)
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
   });
 });
