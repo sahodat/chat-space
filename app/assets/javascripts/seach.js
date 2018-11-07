a32a2ce8a02951790d798e4e3a653588ac69b88c
sahodat <7207t251s906@gmail.com>
2018-11-07 17:20:08 +0900

    make html

--- app/assets/javascripts/seach.js
+++ app/assets/javascripts/seach.js
@@ -1,4 +1,14 @@
 $(function() {
+
+  var user_result = $("#user-search-result");
+  function appenduser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                </div>`
+                user_result.append(html);
+  }
+
   $('#user-search-field').on("keyup", function() {
     var input = $('#user-search-field').val();
     console.log(this)
@@ -7,6 +17,8 @@ $(function() {
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
+
     })
+    });
   });
 });
