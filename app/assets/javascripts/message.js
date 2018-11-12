16a3ed663830021e46c40e8f7e856b2a0773a195
sahodat <7207t251s906@gmail.com>
2018-11-12 17:52:08 +0900

    11/12-2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,46 +1,55 @@
     $(function(){
-   // function buildHTML(message){
-      var html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${ message.user_name }
-                      </div>
-                      <div class="upper-message__date">
-                        ${ message.created_at }
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${ message.content }
-                      </p>
-                      ${image}
-                    </div>
-                  </div>`;
-      var image = message.image_url? `<img src="${message.image_url}" class='lower-message__image'>` : "";
-      return html;
-    }
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
-      var url = $(this).attr('action');
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-        contentType: false,
-      })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $('.form__message').val('');
-        // $(".message").animate({scrollTop:$('.message').offset().top
-        // });
 
+  function buildHTML(message){
+    if (message.image_url || message.content){
+  var image = message.image_url ? `<img src="${message.image_url}" class='lower-message__image'>` : "";
+  var html = `<div class="message">
+              <div class="upper-message">
+                <div class="upper-message__user-name">
+                  ${message.user_name}
+                </div>
+                <div class="upper-message__date">
+                  ${message.created_at}
+                </div>
+              <div class="lower-message">
+                <p class="lower-message__content">
+                   ${message.content}
+                </p>
+              </div>
+              ${image}
+              </div>`;
+  return html;
+  }else{
+    alert("値が入力されていません");
+  }
+  }
 
-
-      })
+  $('#item_form').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages.js-messages').append(html);
+      $('#item_form')[0].reset();
+      // $(function(){
+      //   $(".messages.js-messages").animate({scrollTop: $(".messages.js-messages")[0].scrollHeight}, 'slow');
+      // })
+    })
+    .fail(function(){
+      alert('error');
+    })
+    .always(function(){
+      $('.form__submit').prop("disabled", false);
+    })
+  });
+});
+
