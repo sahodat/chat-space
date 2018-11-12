6a5009e28e1176279f9d2455c35845b341dbb719
sahodat <7207t251s906@gmail.com>
2018-11-12 19:10:47 +0900

    11/12-4

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,10 +5,11 @@ class Group < ApplicationRecord
   has_many :messages
 
 
+def show_last_message
     if (last_message = messages.last).present?
       last_message.content? ? last_message.content : '画像が投稿されています'
     else
       'まだメッセージはありません。'
     end
-  end
+end
 end
